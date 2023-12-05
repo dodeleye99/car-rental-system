e67956f33db70d9dc5efc7da71a7539f516c4383
dodeleye099 <109389802+dodeleye99@users.noreply.github.com>
2023-12-05 23:04:32 +0000

    Update main.py to finish implementing init()
    The init() function (now called init_objects() for better clarity)
    has now been properly implemented. It prompts the user to enter
    a numerical identifier (which is validated), which is then used
    to create a Customer object.
    
    The function now returns a tuple containing the Customer object
    followed by the RentalShop object created earlier, so that it can
    be accessed by the main() function.

--- main.py
+++ main.py
@@ -2,15 +2,47 @@ from customer import Customer
 from rental_shop import RentalShop
 
 
-def init():
+def init_objects():
 	"""
-	Used to initialise customer and rental shop objects
-	:return:
+	Used to initialise customer and rental shop objects.
+	The user will be allowed to enter an identifier to use for the customer.
+
+	:return: a tuple consisting of a Customer object followed by a RentalShop object.
 	"""
 
+	# Initialise a car shop with identifier 'shop1'
 	car_shop = RentalShop("shop1")
 
-	print("Initialising...(still incomplete)")
+	# Create a variable that will store the customer number inputted by the user.
+	customer_number = "000000"
+
+	# Use a flag for validating the user's input for the customer number.
+	validated = False
+
+	# As long as validation is not complete, have the user enter their customer number.
+	while not validated:
+
+		# Prompt the user to enter their customer number.
+		customer_number = input("Please enter your six-digit customer number: ")
+
+		# Validation #1: make sure that their input consists of only digits
+		if not customer_number.isdigit():
+			print("Invalid format (must contain only digits)\n")
+
+		# Validation #1: make sure that their input consists of six characters (i.e. a 6-digit number)
+		elif len(customer_number) != 6:
+			print("Invalid format (must be a six-digit number)\n")
+
+		# At this point, validation is complete, so prepare to end the loop.
+		else:
+			print("Customer number accepted!")
+			validated = True
+
+	# Instantiate a Customer object with the inputted customer number.
+	customer = Customer(customer_number)
+
+	# Output the newly created Customer and RentalShop objects (as a tuple).
+	return customer, car_shop
 
 
 def inquire():
@@ -42,13 +74,13 @@ def main():
 	Represents the main entry point of the program, immediately run when this script is executed
 	"""
 
-	print("Hello! Welcome to the Car Rental System")
+	print("Hello! Welcome to the Car Rental System\n")
 
 	# Create a flag do determine whether the main loop should still br running
 	running = True
 
 	# Initialise the customer and the rental shop data
-	init()
+	customer, rental_shop = init_objects()
 
 	# The main loop
 	while running:
